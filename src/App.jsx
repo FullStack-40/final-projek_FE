f08ab49a5828e610c766b0b1df3c82f80010bf3c
fazriegi <fazriegi80@gmail.com>
2023-11-14 20:44:21 +0700

    add react router

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,16 @@
+import { Route, Routes } from "react-router-dom";
+import Login from "./pages/login";
+import Register from "./pages/register";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
