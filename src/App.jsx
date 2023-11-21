5b4dc7b14ca3e52c3c8abbe7d5008933ab4e5f3e
deknaa <kadekna20@gmail.com>
2023-11-21 19:39:52 +0800

    updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React from "react";
 import ArticleUser from "./pages/ArticleUser";
+import ArticleDetails from "./components/article_user/ArticleDetails";
+import PilihDokter from "./pages/PilihDokter";
 
 function App() {
   return <>
-    <ArticleUser />
+    <PilihDokter />
   </>;
 }
 
