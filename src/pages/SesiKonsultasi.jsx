670b2258c63ef7a2ecb440788b01751af9410a84
deknaa <kadekna20@gmail.com>
2023-11-22 23:27:20 +0800

    Finished Konsultasi Page

---
+++ src/pages/SesiKonsultasi.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import NavbarArticle from '../components/article_user/NavbarArticle'
+import FilterKonsultasi from '../components/Konsultasi/FilterKonsultasi'
+import DataKonsultasi from '../components/Konsultasi/DataKonsultasi'
+
+const SesiKonsultasi = () => {
+  return (
+    <>
+        <NavbarArticle />
+        <FilterKonsultasi />
+        <DataKonsultasi />
+    </>
+  )
+}
+
+export default SesiKonsultasi
