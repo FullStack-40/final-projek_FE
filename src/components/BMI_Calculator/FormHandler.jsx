18a7678fc60f271a97bd8bb02c662b594e5aac7d
deknaa <kadekna20@gmail.com>
2023-11-24 15:47:14 +0800

    Styling Button Components

--- src/components/BMI_Calculator/FormHandler.jsx
+++ src/components/BMI_Calculator/FormHandler.jsx
@@ -3,16 +3,20 @@ import React from 'react'
 const FormHandler = () => {
   return (
     <>
-        <form action="#">
-            <label htmlFor="tinggiBadan">Tinggi Badan (Cm)</label>
-            <input type="number" name="tinggiBadan" id="tinggiBadan" />
-            <label htmlFor="beratBadan">Berat Badan (Kg)</label>
-            <input type="number" name="beratBadan" id="beratBadan" />
-            <label htmlFor="usia">Usia Anda</label>
-            <input type="number" name="usia" id="usia" />
-            <label htmlFor="hasil">Hasil Perhitungan</label>
-            <input type="number" name="hasil" id="hasil" />
-        </form>
+        <div className='ml-[25%] mr-[25%] mt-16 md:ml-[35%] md:mr-[35%]'>
+            <form action="#" className='flex flex-col'>
+                <label htmlFor="tinggiBadan">Tinggi Badan (CM)</label>
+                <input type="number" name="tinggiBadan" id="tinggiBadan" className='border py-2 px-5 rounded mt-3 mb-3'/>
+                <label htmlFor="beratBadan">Berat Badan (Kg)</label>
+                <input type="number" name="beratBadan" id="beratBadan" className='border py-2 px-5 rounded mt-3 mb-3'/>
+                <label htmlFor="usia">Usia Anda</label>
+                <input type="number" name="usia" id="usia" className='border py-2 px-5 rounded mt-3 mb-3'/>
+                <label htmlFor="hasil">Hasil Perhitungan</label>
+                <input type="number" name="hasil" id="hasil" className='border py-2 px-5 rounded mt-3 mb-3'/>
+                <div className='flex gap-10w'>
+                </div>
+            </form>
+        </div>
     </>
   )
 }
--- src/pages/CalculatorBMI.jsx
+++ src/pages/CalculatorBMI.jsx
@@ -1,10 +1,14 @@
 import React from 'react'
 import NavbarArctile from '../components/article_user/NavbarArticle'
+import FormHandler from '../components/BMI_Calculator/FormHandler'
+import ButtonForm from '../components/BMI_Calculator/buttonForm'
 
 const CalculatorBMI = () => {
   return (
     <>
         <NavbarArctile />
+        <FormHandler />
+        <ButtonForm />
     </>
   )
 }
