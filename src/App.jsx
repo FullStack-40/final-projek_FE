2f430d8b56e6bdcf3ac303c3697e7df08623bd81
deknaa <kadekna20@gmail.com>
2023-11-26 11:04:54 +0800

    Updated app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,12 +3,10 @@ import ArticleUser from "./pages/ArticleUser";
 import SesiKonsultasi from "./pages/SesiKonsultasi";
 import CalculatorBMI from "./pages/CalculatorBMI";
 import ChatKonsultasi from "./pages/ChatKonsultasi";
-import PilihDokter from './pages/PilihDokter'
-import ProfileDoctor from './components/Pilih_Dokter/ProfileDoctor'
 
 function App() {
   return <>
-    <ProfileDoctor />
+    <ChatKonsultasi />
   </>;
 }
 
