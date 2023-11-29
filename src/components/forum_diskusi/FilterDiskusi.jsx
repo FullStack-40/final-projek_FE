f1d0590a16534f4924c68ce0619a45de4ef24107
deknaa <kadekna20@gmail.com>
2023-11-29 10:55:45 +0800

    Added FilterDiskusi

--- src/components/forum_diskusi/FilterDiskusi.jsx
+++ src/components/forum_diskusi/FilterDiskusi.jsx
@@ -1,13 +1,39 @@
 import React from 'react'
 
 const FilterDiskusi = () => {
+
+  const popularTag = [
+    {id: 1, tag: "Gizi"},
+    {id: 2, tag: "Sayuran"},
+    {id: 3, tag: "Sehat"},
+    {id: 4, tag: "Makanansehat"},
+    {id: 5, tag: "kandungangizi"},
+    {id: 6, tag: "polamakan"},
+    {id: 7, tag: "giziseimbang"},
+  ]
+
   return (
     <>
-        <div className='border'>
-            <h2>Urutan Diskusi</h2>
-            <input type="radio" name="terbaru" id="terbaru" value="Terbaru" />
-            <input type="radio" name="terbaru" id="terbaru" value="Terlama" />
-            
+        <div className='border mt-5 w-[20%]'>
+            <h2 className='font-bold text-xl text-center py-3'>Urutan Diskusi</h2>
+            <form className='flex flex-col ml-4 mb-8'>
+              <label htmlFor="terbaru">
+                <input type="radio" name="terbaru" id="terbaru" value="Terbaru" checked={true} />
+                Terbaru
+              </label>
+              <label htmlFor="terlama">
+                <input type="radio" name="terbaru" id="terlama" value="Terlama" />
+                Terlama
+              </label>
+            </form>
+            <h3 className='text-center font-bold text-xl pb-3'>Kata Kunci Populer</h3>
+            <div className='flex flex-wrap justify-center items-center ml-5 mr-5 w-[70%] rounded'>
+              {popularTag.map(tags => (
+                <p key={tags.id} className='bg-primary-color px-3 py-1 text-white mt-2 rounded-full cursor-pointer hover:bg-other-color'>
+                  #{tags.tag}
+                </p>
+              ))}
+            </div>
         </div>
     </>
   )
